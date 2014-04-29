3ef4b41d81a9db3221571ae8ee5b07a02cc76071
Cristi Scheye <cristi.scheye@gmail.com>
2014-04-29 15:11:01 -0700

    parsing query string params and request body params

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'debugger'
 
 class Params
   # use your initialize to merge params from
@@ -6,9 +7,15 @@ class Params
   # 2. post body
   # 3. route params
   def initialize(req, route_params = {})
+    debugger
+    @params = parse_www_encoded_form(req.query_string)
+    @params = @params.merge(parse_www_encoded_form(req.body))
+    debugger
+    a = 1
   end
 
   def [](key)
+    @params[key]
   end
 
   def permit(*keys)
@@ -21,6 +28,7 @@ class Params
   end
 
   def to_s
+    @params.to_s
   end
 
   class AttributeNotFoundError < ArgumentError; end;
@@ -32,10 +40,33 @@ class Params
   # should return
   # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
   def parse_www_encoded_form(www_encoded_form)
+    return {} if www_encoded_form.nil?
+    params_arr = URI::decode_www_form(www_encoded_form)
+
+    params = {}
+
+    params_arr.each do |kv_pair|
+      key, val = kv_pair
+      keys = parse_key(key)
+      current_hash = make_hash(keys, val)
+      params = params.deep_merge(current_hash)
+    end
+    params
   end
 
   # this should return an array
   # user[address][street] should return ['user', 'address', 'street']
   def parse_key(key)
+    key.gsub(']', '').split('[').reverse
+  end
+
+  def make_hash(keys, val)
+    current_key = keys.pop
+    if keys.empty?
+      { current_key => val }
+    else
+      { current_key => make_hash(keys, val) }
+    end
   end
 end
+
