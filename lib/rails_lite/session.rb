1404b2740b1b9effe1f47bb64ed73eafb9012566
Cristi Scheye <cristi.scheye@gmail.com>
2014-04-29 15:06:30 -0700

    adding Session class to keep track of cookies

--- lib/rails_lite/session.rb
+++ lib/rails_lite/session.rb
@@ -1,20 +1,28 @@
 require 'json'
 require 'webrick'
+require 'debugger'
 
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    cookie = req.cookies.select{ |c| c.name == '_rails_lite_app'}.first
+
+    @cookie =  cookie.nil? ? {} : JSON.parse(cookie.value.to_s)
   end
 
   def [](key)
+    @cookie[key]
   end
 
   def []=(key, val)
+    @cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    cookie = WEBrick::Cookie.new('_rails_lite_app', @cookie.to_json)
+    res.cookies << cookie
   end
 end
